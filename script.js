04b071442bc3c2922d2fc92034ed1c26263f0973
KevKujo23 <120726382+KevKujo23@users.noreply.github.com>
2022-12-16 22:37:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,142 @@
+function toggleAudio() {
+  var audio = document.getElementById('audio');
+  var button = document.getElementById('button');
+
+  if (audio.paused) {
+    audio.play();
+    button.innerHTML = 'Pause';
+  } else {
+    audio.pause();
+    button.innerHTML = 'Play';
+  }
+}
+
+function toggleAudioA() {
+  var audioA = document.getElementById('audioA');
+  var buttonA = document.getElementById('buttonA');
+
+  if (audioA.paused) {
+    audioA.play();
+    buttonA.innerHTML = 'Pause';
+  } else {
+    audioA.pause();
+    buttonA.innerHTML = 'Play';
+  }
+}
+
+function toggleAudioB() {
+  var audioB = document.getElementById('audioB');
+  var buttonB = document.getElementById('buttonB');
+
+  if (audioB.paused) {
+    audioB.play();
+    buttonB.innerHTML = 'Pause';
+  } else {
+    audioB.pause();
+    buttonB.innerHTML = 'Play';
+  }
+}
+
+function toggleAudioC() {
+  var audioC = document.getElementById('audioC');
+  var buttonC = document.getElementById('buttonC');
+
+  if (audioC.paused) {
+    audioC.play();
+    buttonC.innerHTML = 'Pause';
+  } else {
+    audioC.pause();
+    buttonC.innerHTML = 'Play';
+  }
+}
+
+function toggleAudioD() {
+  var audioD = document.getElementById('audioD');
+  var buttonD = document.getElementById('buttonD');
+
+  if (audioD.paused) {
+    audioD.play();
+    buttonD.innerHTML = 'Pause';
+  } else {
+    audioD.pause();
+    buttonD.innerHTML = 'Play';
+  }
+}
+
+function toggleAudioE() {
+  var audioE = document.getElementById('audioE');
+  var buttonE = document.getElementById('buttonE');
+
+  if (audioE.paused) {
+    audioE.play();
+    buttonE.innerHTML = 'Pause';
+  } else {
+    audioE.pause();
+    buttonE.innerHTML = 'Play';
+  }
+}
+
+function toggleAudioF() {
+  var audioF = document.getElementById('audioF');
+  var buttonF = document.getElementById('buttonF');
+
+  if (audioF.paused) {
+    audioF.play();
+    buttonF.innerHTML = 'Pause';
+  } else {
+    audioF.pause();
+    buttonF.innerHTML = 'Play';
+  }
+}
+
+function toggleAudioG() {
+  var audioG = document.getElementById('audioG');
+  var buttonG = document.getElementById('buttonG');
+
+  if (audioG.paused) {
+    audioG.play();
+    buttonG.innerHTML = 'Pause';
+  } else {
+    audioG.pause();
+    buttonG.innerHTML = 'Play';
+  }
+}
+
+function toggleAudioH() {
+  var audioH = document.getElementById('audioH');
+  var buttonH = document.getElementById('buttonH');
+
+  if (audioH.paused) {
+    audioH.play();
+    buttonH.innerHTML = 'Pause';
+  } else {
+    audioH.pause();
+    buttonH.innerHTML = 'Play';
+  }
+}
+
+function toggleAudioI() {
+  var audioI = document.getElementById('audioI');
+  var buttonI = document.getElementById('buttonI');
+
+  if (audioI.paused) {
+    audioI.play();
+    buttonI.innerHTML = 'Pause';
+  } else {
+    audioI.pause();
+    buttonI.innerHTML = 'Play';
+  }
+}
+function toggleAudioK() {
+  var audioK = document.getElementById('audioK');
+  var buttonK = document.getElementById('buttonK');
+
+  if (audioK.paused) {
+    audioK.play();
+    buttonK.innerHTML = 'Pause';
+  } else {
+    audioK.pause();
+    buttonK.innerHTML = 'Play';
+  }
+}
+
